a1a94a70df35953692a48137bdb6cffd53e5ddd0
Joshua Rodriguez <joshuadrp@hotmail.com>
2024-08-27 01:39:42 +0000

    Add location model to core app.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
