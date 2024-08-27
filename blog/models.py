626fb8e52ef13baca22ce51c37d8f8a4cd31dadb
Joshua Rodriguez <joshuadrp@hotmail.com>
2024-08-27 19:07:57 +0000

    Add Post model to blog app.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from core.models import Location
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
+    img_url = models.CharField(max_length=200, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(
+    User, on_delete=models.CASCADE)
+    location_id = models.ForeignKey(
+        Location, on_delete=models.CASCADE
+    )
+
+    class Meta:
+        ordering = ["created_on"]
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
