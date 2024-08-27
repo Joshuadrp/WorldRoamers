28655d83051ae7426568d52dafa1b0aab1b724bb
Joshua Rodriguez <joshuadrp@hotmail.com>
2024-08-27 19:52:55 +0000

    Update views.py in core app to enhance performance.

--- core/views.py
+++ core/views.py
@@ -4,4 +4,5 @@ from .models import Location
 
 # Create your views here.
 class Locations(generic.ListView):
-    model = Location
+    queryset = Location.objects.all() #makes easier to get data.
+    template_name = "location_list.html"
