ec5849e623fdf35cebdc39f224486530415b6f30
Joshua Rodriguez <joshuadrp@hotmail.com>
2024-08-27 23:23:01 +0000

    Add summernote to blog app.

--- world_roamers/urls.py
+++ world_roamers/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', include('core.urls'), name='core-urls'),
     path('blog/', include('blog.urls'), name = 'blog-urls'),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
 ]
