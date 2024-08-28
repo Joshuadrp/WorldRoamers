eb28f2e1aea6e5f6ac65818c5453886c9e59f130
Joshua Rodriguez <joshuadrp@hotmail.com>
2024-08-28 02:09:43 +0000

    Add location based filtering for blog posts.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.Posts.as_view(), name='blog'), #django will append the empty string using urls.py in project.
     path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('location/<slug:location_slug>/', views.PostsByLocation.as_view(), name='posts_by_location'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
+from core.models import Location
 
 # Create your views here.
 class Posts(generic.ListView):
     queryset = Post.objects.all()
     template_name = "blog/posts.html"
+
+class PostsByLocation(generic.ListView):
+    template_name = "blog/posts.html"
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        location_slug = self.kwargs.get('location_slug')
+        location = get_object_or_404(Location, slug=location_slug)
+        return Post.objects.filter(location=location)
   
 def post_detail(request, slug):
     """
@@ -29,5 +39,7 @@ def post_detail(request, slug):
         "blog/post_detail.html",
         {"post": post},
     )
+
+
 
 
