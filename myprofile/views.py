1b6f7b5df82702776864a1ce55c97e6ade6c8f8a
Joshua Rodriguez <joshuadrp@hotmail.com>
2024-09-15 11:15:17 +0000

    Update models for myprofile app: If user has a bio it is displayed, if not feeback is provided.

--- myprofile/views.py
+++ myprofile/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
+from .models import MyProfile
 
 # Create your views here.
-class MyProfile(generic.TemplateView):
+class MyProfile(generic.ListView):
+    queryset = MyProfile.objects.all()
     template_name = "myprofile/myprofile.html"
