53075a17b37d43d8e03cbdef023a0c397601e99a
Joshua Rodriguez <joshuadrp@hotmail.com>
2024-09-15 12:48:31 +0000

    Update models in myprofile app: Default value removed.

--- myprofile/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import MyProfile
-
-class MyProfileForm(forms.ModelForm):
-    class Meta:
-        model = MyProfile
-        fields = ['bio', 'name', 'surname', 'profile_picurl', 'countries_visited', 'interests']
--- myprofile/models.py
+++ myprofile/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import User
 class MyProfile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
-    name = models.CharField(max_length=200, unique=True, blank=True, default='')  # Default value
-    surname = models.CharField(max_length=200, unique=True, blank=True, default='')  # Default value
+    name = models.CharField(max_length=200, unique=True, blank=True)  
+    surname = models.CharField(max_length=200, unique=True, blank=True)  
     profile_picurl = models.CharField(max_length=200, blank=True)
-    countries_visited = models.CharField(max_length=200, blank=True, default='How many?!')  # Default value
-    interests = models.CharField(max_length=200, blank=True, default='Do you have any?')  # Default value
+    countries_visited = models.CharField(max_length=200, blank=True)  
+    interests = models.CharField(max_length=200, blank=True) 
 
     def __str__(self):
         return str(self.user)
