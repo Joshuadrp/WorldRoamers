5251fab1ba7ea038b7a3feb4e22fb12ced937ca7
Joshua Rodriguez <joshuadrp@hotmail.com>
2024-09-15 11:36:57 +0000

    Update models table in myprofile app: Showing different fields such as Name, Surname, Interests, etc.

--- myprofile/models.py
+++ myprofile/models.py
@@ -2,10 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class MyProfile(models.Model):
-    user = models.OneToOneField(User,null=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
+    name = models.CharField(max_length=200, unique=True, blank=True, default='')  # Default value
+    surname = models.CharField(max_length=200, unique=True, blank=True, default='')  # Default value
+    profile_picurl = models.CharField(max_length=200, blank=True)
+    countries_visited = models.CharField(max_length=200, unique=True, blank=True, default='')  # Default value
+    interests = models.CharField(max_length=200, unique=True, blank=True, default='')  # Default value
 
     def __str__(self):
         return str(self.user)
-
-    
