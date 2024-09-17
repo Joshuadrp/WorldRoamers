1dc389e2ad097bd05b31b1b89030b7958fbf5d19
Joshua Rodriguez <joshuadrp@hotmail.com>
2024-09-17 10:24:57 +0000

    Add nationality to models.py in my profile app: Also included field in forms.oy.

--- myprofile/models.py
+++ myprofile/models.py
@@ -9,6 +9,7 @@ class MyProfile(models.Model):
     profile_picurl = models.CharField(max_length=200, blank=True)
     countries_visited = models.CharField(max_length=200, blank=True)  
     interests = models.CharField(max_length=200, blank=True) 
+    nationality =  models.CharField(max_length=200, blank=True) 
 
     def __str__(self):
         return str(self.user)
