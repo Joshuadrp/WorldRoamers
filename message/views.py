701c69e27d5bade84011a32755cfa7913a569392
Joshua Rodriguez <joshuadrp@hotmail.com>
2024-09-19 21:14:51 +0000

    Update posts.html: Try to style, more work needed.

--- message/views.py
+++ message/views.py
@@ -7,7 +7,7 @@
 
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
+# from django.contrib import messages
 from .models import Messages
 from .forms import MessageForm
 
